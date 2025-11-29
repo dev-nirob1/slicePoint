22601a9361ef770e9a3bfb27744ea05549b3a91e
dev-nirob1 <dev.nirob1@gmail.com>
2025-11-29 14:51:24 +0600

    hero section

--- src/zems/Front/Components/Section/HeroSection.vue
+++ src/zems/Front/Components/Section/HeroSection.vue
@@ -2,7 +2,47 @@
 </script>
 
 <template>
-  <BaseTitle>Hero section</BaseTitle>
+  <section class="hero flex-center relative">
+    <div class="container">
+      <div class="medium-2 align-center gap-2">
+        <div class="content">
+          <HeroTitle>
+            Experience The Authentic Pepperoni Punch
+          </HeroTitle>
+          <BaseParagraph> Hand Stretched dough, slow-cooked italian tomato-sauce and smoky pepperoni wood fired to
+            golden perfection.
+          </BaseParagraph>
+          <div class="flex gap-1">
+            <BaseButton>Order Now</BaseButton>
+            <BaseButton>View Ingredients</BaseButton>
+          </div>
+        </div>
+        <div class="image">
+          <BaseImage image="/hero.jpg" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <style scoped>
+.hero {
+  min-height: 100vh;
+  color: var(--white-color);
+}
+
+.hero p {
+  font-size: 1.1rem;
+}
+
+.hero .image {
+  position: absolute;
+  inset: 0;
+  z-index: -1;
+}
+.image::before{
+  content: '';
+  position: absolute;
+  inset: 0;
+  background: rgba(0,0,0, .2)
+}
 </style>
